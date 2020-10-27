97b9456d9f9a2b3c8861cfe170b158c61b20ac09
James Karsten <james.e.karsten@gmail.com>
2020-10-26 21:47:31 -0400

    testing JS header & footer

---
+++ main.js
@@ -0,0 +1,28 @@
+var header = '<header>\
+<div id="head">\
+    <a href="index.html"><span id="name-first">James</span><span id="name-last">Karsten</span></a>\
+</div>\
+<div id="navigation">\
+    <div class="dropdown">\
+        <a href="photography.html" class="nav-level-1">Photography</a>\
+        <div class="nav-level-2">\
+            <a href="portrait">Portrait</a>\
+            <a href="nature.html">Nature & Urban</a>\
+            <a href="film.html">B&W Film</a>\
+        </div>\
+    </div>\
+    <a href="about.html" class="nav-level-1">About</a>\
+    <a href="contact.html" class="nav-level-1">Contact</a>\
+</div>\
+</header>';
+
+var footer = '<footer> \
+<div id="copyright">\
+    Copyright &copy; 2020 James Karsten. All rights reserved.\
+</div>\
+</footer>';
+
+
+
+document.getElementById("headerInclude").innerHTML=header
+document.getElementById("footerInclude").innerHTML=footer
