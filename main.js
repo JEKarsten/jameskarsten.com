3119fa148aba934de36c3803822e2aa43f4fccbd
James Karsten <james.e.karsten@gmail.com>
2020-10-27 04:04:12 -0400

    Updating JS header/footer on About

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
-var header = '<header>\
+var header = '\
 <div id="head">\
-    <a href="/"><span id="name-first">James</span><span id="name-last">Karsten</span></a>\
+  <a href="/"><span id="name-first">James</span><span id="name-last">Karsten</span></a>\
 </div>\
 <div id="navigation">\
-    <div class="dropdown">\
-        <a href="/photography" class="nav-level-1">Photography</a>\
-        <div class="nav-level-2">\
-            <a href="/photography/composite">Composite</a>\
-            <a href="/photography/portrait">Portrait</a>\
-            <a href="/photography/nature-urban">Nature & Urban</a>\
-            <a href="/photography/film">Black & White Film</a>\
-        </div>\
+  <div class="dropdown">\
+    <a href="/photography" class="nav-level-1">Photography</a>\
+    <div class="nav-level-2">\
+      <a href="/photography/composite">Composite</a>\
+      <a href="/photography/portrait">Portrait</a>\
+      <a href="/photography/nature-urban">Nature & Urban</a>\
+      <a href="/photography/film">Black & White Film</a>\
     </div>\
-    <div class="dropdown">\
-        <a href="/design" class="nav-level-1">Design</a>\
-        <div class="nav-level-2">\
-            <a href="/design/print">Print Media</a>\
-            <a href="/design/logos">Logos</a>\
-        </div>\
+  </div>\
+  <div class="dropdown">\
+    <a href="/design" class="nav-level-1">Design</a>\
+    <div class="nav-level-2">\
+      <a href="/design/print">Print Media</a>\
+      <a href="/design/logos">Logos</a>\
     </div>\
-    <a href="/coding" class="nav-level-1">Coding</a>\
-    <a href="/about" class="nav-level-1">About</a>\
-</div>\
-</header>';
+  </div>\
+  <a href="/coding" class="nav-level-1">Coding</a>\
+  <a href="/about" class="nav-level-1">About</a>\
+</div>';
 
-var footer = '<footer> \
+var footer = '\
 <div id="copyright">\
-    Copyright &copy; 2020 James Karsten. All rights reserved.\
-</div>\
-</footer>';
+  Copyright &copy; 2020 James Karsten. All rights reserved.\
+</div>';
 
 var fav = document.createElement("link");
 fav.setAttribute("rel", "icon");
@@ -38,5 +36,6 @@ fav.setAttribute("type", "image/svg");
 
 document.head.appendChild(fav);
 document.getElementsByTagName("header")[0].innerHTML = header;
+document.getElementsByTagName("footer")[0].innerHTML = footer;
 document.getElementById("headerInclude").innerHTML = header;
 document.getElementById("footerInclude").innerHTML = footer;
