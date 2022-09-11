0116e90c09aa87006e2a38ef7941266753af25d6
James Karsten <james.e.karsten@gmail.com>
2022-09-10 20:52:46 -0400

    updating coding

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
+// var header = '\
+// <div id="head">\
+//   <a href="/"><span id="name-first">James</span><span id="name-last">Karsten</span></a>\
+// </div>\
+// <div id="navigation">\
+//   <div class="dropdown">\
+//     <a href="/photography" class="nav-level-1">Photography</a>\
+//     <div class="nav-level-2">\
+//       <a href="/photography/portrait">Portrait</a>\
+//       <a href="/photography/landscape-urban">Landscape & Urban</a>\
+//       <a href="/photography/surrealism">Surrealism</a>\
+//       <a href="/photography/film">Black & White Film</a>\
+//     </div>\
+//   </div>\
+//   <div class="dropdown">\
+//     <a href="/design" class="nav-level-1">Design</a>\
+//     <div class="nav-level-2">\
+//       <a href="/design/print">Print Media</a>\
+//       <a href="/design/logos">Logos</a>\
+//     </div>\
+//   </div>\
+//   <a href="/coding" class="nav-level-1">Coding</a>\
+//   <a href="/about" class="nav-level-1">About</a>\
+// </div>';
+
 var header = '\
 <div id="head">\
   <a href="/"><span id="name-first">James</span><span id="name-last">Karsten</span></a>\
 </div>\
 <div id="navigation">\
-  <div class="dropdown">\
-    <a href="/photography" class="nav-level-1">Photography</a>\
-    <div class="nav-level-2">\
-      <a href="/photography/portrait">Portrait</a>\
-      <a href="/photography/landscape-urban">Landscape & Urban</a>\
-      <a href="/photography/surrealism">Surrealism</a>\
-      <a href="/photography/film">Black & White Film</a>\
-    </div>\
-  </div>\
-  <div class="dropdown">\
-    <a href="/design" class="nav-level-1">Design</a>\
-    <div class="nav-level-2">\
-      <a href="/design/print">Print Media</a>\
-      <a href="/design/logos">Logos</a>\
-    </div>\
-  </div>\
   <a href="/coding" class="nav-level-1">Coding</a>\
   <a href="/about" class="nav-level-1">About</a>\
 </div>';
 
 var footer = '\
 <div id="copyright">\
-  Copyright &copy; 2021 James Karsten. All rights reserved.\
+  Copyright &copy; 2022 James Karsten. All rights reserved.\
 </div>';
 
 
